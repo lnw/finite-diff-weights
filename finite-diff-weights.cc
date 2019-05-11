481915bb9aa7ade247f40c4a43341627ee69efa1
Lukas N Wirz <l.n.wirz@kjemi.uio.no>
2019-05-12 01:15:14 +0300

    should work now

--- finite-diff-weights.cc
+++ finite-diff-weights.cc
@@ -1,77 +1,102 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <numeric>
+#include <cassert>
+
+#include "auxiliary.hh"
 
 using namespace std;
 
+// M: maximum derivative
+// N: maximum accuracy
+// x0: position at which value/derivative is approximated
+// alpha: grid point position
+vector<vector<vector<double>>> get_weights(const int M, const int N, const double x0, const vector<double> alpha){
 
-void get_weights(const int M, const int N, const double x0, const double * alpha, double delta[][16][5]){
-
+  vector<vector<vector<double>>> delta(M+1, vector<vector<double>>(N+1, vector<double>(N+1, 0.0)));
   delta[0][0][0] = 1.0;
   double c1 = 1.0;
   for (int n=1; n<=N; n++){
     double c2 = 1.0;
-    
+
     for (int nu=0; nu<n; nu++){
       double c3 = alpha[n] - alpha[nu];
       c2 *= c3;
-    
-      if (n<=M) delta[n-1][nu][n] = 0;
+
+      // if (n<=M) delta[n][n-1][nu] = 0.0;
       for (int m=0; m<=min(n,M); m++){
         // cout << "c3: " << c3 << endl;
-        if(m==0) delta[n][nu][m] = ((alpha[n] - x0)*delta[n-1][nu][m]) / c3;
-        else delta[n][nu][m] = ((alpha[n] - x0)*delta[n-1][nu][m] - m*delta[n-1][nu][m-1]) / c3;
+        if(m==0) delta[m][n][nu] = ((alpha[n] - x0)*delta[m][n-1][nu]) / c3;
+        else delta[m][n][nu] = ((alpha[n] - x0)*delta[m][n-1][nu] - m*delta[m-1][n-1][nu]) / c3;
         // cout << delta[n][nu][m] << endl;
-        cout << "writing " << n << ", " << nu << ", " << m << endl;
+        // cout << "writing " << n << ", " << nu << ", " << m << endl;
       }
     }
     for (int m=0; m<=min(n,M); m++){
-      if (m==0) delta[n][n][m] = c1/c2 * ( - (alpha[n-1] - x0)*delta[n-1][n-1][m]);
-      else delta[n][n][m] = c1/c2 * (m*delta[n-1][n-1][m-1] - (alpha[n-1] - x0)*delta[n-1][n-1][m]);
-        cout << "writing " << n << ", " << n << ", " << m << endl;
+      if (m==0) delta[m][n][n] = c1/c2 * ( - (alpha[n-1] - x0)*delta[m][n-1][n-1]);
+      else delta[m][n][n] = c1/c2 * (m*delta[m-1][n-1][n-1] - (alpha[n-1] - x0)*delta[m][n-1][n-1]);
+      // cout << "writing " << n << ", " << n << ", " << m << endl;
     }
     c1 = c2;
   }
+
+  return delta;
 }
 
 
 int main(int ac, char **av) {
 
   const int M = 4; // max derivative
-  const int N = 8; // number of points
-  const double x0 = -4.0;
-  // double alpha[9] = {0,-1,1,-2,2,-3,3,-4,4};
-  double alpha[9] = {-4, -3, -2, -1,0,1,2,3,4};
-  double delta[2*N][2*N][M+1];
+  const int N = 8; // will calculate all accuracies until n+1
+  const double x0 = -3.5;
+  vector<double> alpha({-4, -3, -2, -1, 0, 1, 2, 3, 4});
+  assert(alpha.size() > N);
+  vector<double> alpha_sorted(alpha);
 
-  get_weights(M, N, x0, alpha, delta);
+  // alpha should be sorted ascendingly
+  sort(&alpha[0], &alpha[0]+N+1);
 
-  cout << "out" << endl;
-//  cout << delta[0][0][0] << ", " << delta[0][1][0] << ", " << delta[0][2][0] << ", " << delta[0][3][0] << ", " << delta[0][4][0] << ", " << delta[0][5][0]<< ", " << delta[0][6][0] << endl;
-//  cout << delta[1][0][0] << ", " << delta[1][1][0] << ", " << delta[1][2][0] << ", " << delta[1][3][0] << ", " << delta[1][4][0] << ", " << delta[1][5][0]<< ", " << delta[1][6][0] << endl;
-//  cout << delta[2][1][0] << ", " << delta[2][2][0] << ", " << delta[2][3][0] << ", " << delta[2][4][0] << ", " << delta[2][5][0]<< ", " << delta[2][6][0] << endl;
-//  cout << delta[3][1][0] << ", " << delta[3][2][0] << ", " << delta[3][3][0] << ", " << delta[3][4][0] << ", " << delta[3][5][0]<< ", " << delta[3][6][0] << endl;
-//  cout << delta[4][1][0] << ", " << delta[4][2][0] << ", " << delta[4][3][0] << ", " << delta[4][4][0] << ", " << delta[4][5][0]<< ", " << delta[4][6][0] << endl;
-//  cout << delta[5][1][0] << ", " << delta[5][2][0] << ", " << delta[5][3][0] << ", " << delta[5][4][0] << ", " << delta[5][5][0]<< ", " << delta[5][6][0] << endl;
-//  cout << delta[6][1][0] << ", " << delta[6][2][0] << ", " << delta[6][3][0] << ", " << delta[6][4][0] << ", " << delta[6][5][0]<< ", " << delta[6][6][0] << endl << endl;
-                                              
-  cout << delta[0][0][1] << ", " <<delta[0][1][1] << ", " << delta[0][2][1] << ", " << delta[0][3][1] << ", " << delta[0][4][1] << ", " << delta[0][5][1]<< ", " << delta[0][6][1] << endl;
-  cout << delta[1][0][1] << ", " <<delta[1][1][1] << ", " << delta[1][2][1] << ", " << delta[1][3][1] << ", " << delta[1][4][1] << ", " << delta[1][5][1]<< ", " << delta[1][6][1] << endl;
-  cout << delta[2][0][1] << ", " <<delta[2][1][1] << ", " << delta[2][2][1] << ", " << delta[2][3][1] << ", " << delta[2][4][1] << ", " << delta[2][5][1]<< ", " << delta[2][6][1] << endl;
-  cout << delta[3][1][1] << ", " << delta[3][2][1] << ", " << delta[3][3][1] << ", " << delta[3][4][1] << ", " << delta[3][5][1]<< ", " << delta[3][6][1] << endl;
-  cout << delta[4][1][1] << ", " << delta[4][2][1] << ", " << delta[4][3][1] << ", " << delta[4][4][1] << ", " << delta[4][5][1]<< ", " << delta[4][6][1] << endl;
-  cout << delta[5][1][1] << ", " << delta[5][2][1] << ", " << delta[5][3][1] << ", " << delta[5][4][1] << ", " << delta[5][5][1]<< ", " << delta[5][6][1] << endl;
-  cout << delta[6][1][1] << ", " << delta[6][2][1] << ", " << delta[6][3][1] << ", " << delta[6][4][1] << ", " << delta[6][5][1]<< ", " << delta[6][6][1] << endl;
-  cout << delta[7][1][1] << ", " << delta[7][2][1] << ", " << delta[7][3][1] << ", " << delta[7][4][1] << ", " << delta[7][5][1]<< ", " << delta[7][6][1] << endl;
-  cout << delta[8][1][1] << ", " << delta[8][2][1] << ", " << delta[8][3][1] << ", " << delta[8][4][1] << ", " << delta[8][5][1]<< ", " << delta[8][6][1] << endl << endl;
-                                                                                      
+  // but get_weights works on an alpha with increasing distance from x0
+  for (double &d: alpha_sorted) d -= x0;
+  sort(&alpha_sorted[0], &alpha_sorted[0]+N+1, abs_comp);
+  for (double &d: alpha_sorted) d += x0;
+  // cout << alpha_sorted << endl;
 
-  double foo = 3/4;
-  double bar = 3.0/4;
-  cout << foo << endl;
-  cout << bar << endl;
+  vector<double> map(N+1);
+  for (int i=0; i<N+1; i++){
+    for (int j=0; j<N+1; j++){
+      if(alpha[j] == alpha_sorted[i]) map[i] = j;
+    }
+  }
+  // cout << map << endl;
 
+  vector<vector<vector<double>>> delta;
+  delta = get_weights(M, N, x0, alpha_sorted);
+
+  cout << "x0: " << x0 << endl;
+  cout << "alpha: " << alpha << endl << endl;
+
+//   cout << delta << endl;
+
+  // reorder according to ordered alpha
+  for(int m=0; m<M+1; m++){
+    for(int j=0; j<N+1; j++){
+      vector<double> tmp(N+1);
+      for (int k=0; k<N+1; k++){
+        tmp[map[k]] = delta[m][j][k];
+      }
+      delta[m][j] = tmp;
+    }
+  }
+
+  cout << "finite difference weights of grid points alpha (in the order of alpha):" << endl;
+  for (int i=0; i<M+1; i++){
+    cout << "derivative order: " << i << endl;
+    for (int j=i; j<N+1; j++){
+      cout << setprecision(12) << "accuracy order " << j-i+1 << ": " << delta[i][j] << endl;
+    }
+    cout << endl;
+  }
 
   return 0;
 }
